b4ab8014549025f03913863a8427907e7ab746fb
rodrigocondoritorres-lgtm <rodrigocondoritorres@gmail.com>
2026-09-15 00:41:53 -0500

    Add files via upload

---
+++ C.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    if (n % 2 == 0) {
+        cout << "Par";
+    }
+    else {
+        cout << "Impar";
+    }
+
+    return 0;
+}
