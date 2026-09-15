6dba87c3b3f4694fd066c2fe9594620b9afb0c72
rodrigocondoritorres-lgtm <rodrigocondoritorres@gmail.com>
2026-09-15 00:40:22 -0500

    Add files via upload

---
+++ A.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char C1, C2;
+    cin >> C1 >> C2;
+
+    if (C1 == C2) {
+        cout << "Iguales";
+    }
+    else {
+        cout << "Diferentes";
+    }
+
+    return 0;
+}
